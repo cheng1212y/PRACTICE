82a2d9f5690ca825b585b02b33a5a5bf9daea4e6
cheng1212y <cheng12y@yeah.net>
2020-05-04 17:49:56 +0800

    更新

---
+++ LeetCode/167_LeetCode/src/Test.java
@@ -0,0 +1,39 @@
+/**
+ * @Description:  两数之和 求下标
+给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+说明:
+返回的下标值（index1 和 index2）不是从零开始的。
+你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+示例:
+输入: numbers = [2, 7, 11, 15], target = 9
+输出: [1,2]
+解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted
+ * @Create: 2020-05-04 17:27
+ **/
+public class Test {
+
+
+    public int[] twoSum(int[] numbers, int target) {
+        if (numbers == null) {
+            return null;
+        }
+        int i = 0, j = numbers.length - 1;
+        while (i < j) {
+            int sum = numbers[i] + numbers[j];
+            if (sum == target) {
+                return new int[]{i + 1, j + 1};
+            } else if (sum < target) {             //排序数组 小了就左指针移动 每次在增加
+                i++;
+            } else {
+                j--;                //打了右指针移动  每次在递减
+            }
+        }
+        return null;
+    }
+
+
+
+}
