340cbf7ec890e848ef25fceab7ab1300439830ff
cheng1212y <cheng12y@yeah.net>
2020-05-09 00:20:54 +0800

    更新

---
+++ LeetCode/509_LeetCode/src/Test.java
@@ -0,0 +1,39 @@
+/**
+ * @Description:  斐波那契
+ *  斐波那契数，通常用 F(n) 表示，形成的序列称为斐波那契数列。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+ * F(0) = 0,   F(1) = 1
+ * F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+ * 给定 N，计算 F(N)。
+ * 输入：2
+ * 输出：1
+ * 解释：F(2) = F(1) + F(0) = 1 + 0 = 1.
+ * 示例 2：
+ * 0 ≤ N ≤ 30
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/fibonacci-number
+ * @Create: 2020-05-09 00:14
+ **/
+public class Test {
+
+    public int fib(int N) {
+        if (N <= 1) {
+            return N;
+        }
+        if (N == 2) {
+            return 1;
+        }
+
+        int current = 0;
+        int prev1 = 1;
+        int prev2 = 1;
+
+        for (int i = 3; i <= N; i++) {
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return current;
+    }
+
+
+}
