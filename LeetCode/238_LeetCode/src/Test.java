d7fbf9a6328894e5de9209382a4336a77cfc5605
cheng1212y <cheng12y@yeah.net>
2020-07-22 23:50:18 +0800

    更新

---
+++ LeetCode/238_LeetCode/src/Test.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+/**
+ * @Description: 给你一个长度为 n 的整数数组 nums，其中 n > 1，返回输出数组 output ，其中 output[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积。
+ * 示例:
+ * 输入: [1,2,3,4]
+ * 输出: [24,12,8,6]
+ * 提示：题目数据保证数组之中任意元素的全部前缀元素和后缀（甚至是整个数组）的乘积都在 32 位整数范围内。
+ * 说明: 请不要使用除法，且在 O(n) 时间复杂度内完成此题。
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/product-of-array-except-self
+ * @Create: 2020-07-22 21:10
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(productExceptSelf(new int[]{1, 2, 3, 4,5})));
+    }
+    public static int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        int k = 1;
+        for(int i = 0; i < res.length; i++){
+            res[i] = k;
+            k = k * nums[i]; // 此时数组存储的是除去当前元素左边的元素乘积
+        }
+        k = 1;
+        for(int i = res.length - 1; i >= 0; i--){
+            res[i] *= k; // k为该数右边的乘积。
+            k *= nums[i]; // 此时数组等于左边的 * 该数右边的。
+        }
+        return res;
+    }
+}
