294c8244b9dd86f175c33f668ee5e47ddc0c3fcc
cheng1212y <cheng12y@yeah.net>
2020-07-26 00:05:28 +0800

    更新

---
+++ LeetCode/202_LeetCode/src/Test.java
@@ -0,0 +1,41 @@
+/**
+ * @Description: 编写一个算法来判断一个数 n 是不是快乐数。
+ *
+ * 「快乐数」定义为：对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和，然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。如果 可以变为  1，那么这个数就是快乐数。
+ *
+ * 如果 n 是快乐数就返回 True ；不是，则返回 False 。
+ * 示例：
+ * 输入：19
+ * 输出：true
+ * 解释：
+ * 12 + 92 = 82
+ * 82 + 22 = 68
+ * 62 + 82 = 100
+ * 12 + 02 + 02 = 1
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/happy-number
+ * @Create: 2020-07-25 23:52
+ **/
+public class Test {
+    public class Solution {
+        public int squareSum(int n) {
+            int sum = 0;
+            while(n > 0){
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+            return sum;
+        }
+
+        public boolean isHappy(int n) {
+            int slow = n, fast = squareSum(n);
+            while (slow != fast){
+                slow = squareSum(slow);
+                fast = squareSum(squareSum(fast));
+            };
+            return slow == 1;
+        }
+    }
+
+}
